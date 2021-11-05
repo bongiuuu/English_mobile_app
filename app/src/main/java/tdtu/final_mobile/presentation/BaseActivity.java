8c00ad6d83a884e75b02d2dc05bad426e3de8d1a
hai <bongiuuu2020@gmail.com>
2021-11-05 13:27:40 +0700

    Create a new activity named PracticeActivity

---
+++ app/src/main/java/tdtu/final_mobile/presentation/BaseActivity.java
@@ -0,0 +1,8 @@
+package tdtu.final_mobile.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/tdtu/final_mobile/presentation/PracticeActivity.java
@@ -0,0 +1,22 @@
+package tdtu.final_mobile.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import tdtu.final_mobile.R;
+import tdtu.final_mobile.databinding.ActivityPracticeBinding;
+
+public class PracticeActivity extends BaseActivity {
+    private ActivityPracticeBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPracticeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.tvName.setText("Using Binding");
+    }
+}
