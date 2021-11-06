5958f277b1c1bdfff737060663f50888283e265f
bongiuuu <bongiuuu2020@gmail.com>
2021-11-06 19:28:14 +0700

    i love you ^^~

---
+++ app/src/main/java/tdtu/final_mobile/home/notification/MainNotificationActivity.java
@@ -0,0 +1,15 @@
+package tdtu.final_mobile.home.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import tdtu.final_mobile.R;
+
+public class MainNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_notification);
+    }
+}
