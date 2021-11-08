49f2a5aa6c0459994bffffdd99d6e79d10ba6bb2
hai <bongiuuu2020@gmail.com>
2021-11-08 11:22:16 +0700

    do animation

--- app/src/main/java/tdtu/final_mobile/home/vocabulary/VocabulariesActivity.java
+++ app/src/main/java/tdtu/final_mobile/home/vocabulary/VocabulariesActivity.java
@@ -1,6 +1,11 @@
 package tdtu.final_mobile.home.vocabulary;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import java.util.ArrayList;
 
@@ -33,7 +38,9 @@ public class VocabulariesActivity extends BaseActivity {
                 binding.tvEnglishWord.setText(vocabs.get(currentPosition).getEnglish());
                 binding.tvVietnameseWord.setText(vocabs.get(currentPosition).getVietnamese());
             }
+
             checkVisibleBackAndNextButton(currentPosition);
+            doAnimation();
         });
 
         binding.btnNext.setOnClickListener(view -> {
@@ -43,6 +50,7 @@ public class VocabulariesActivity extends BaseActivity {
                 binding.tvVietnameseWord.setText(vocabs.get(currentPosition).getVietnamese());
             }
             checkVisibleBackAndNextButton(currentPosition);
+            doAnimation();
         });
 
         binding.tvEnglishWord.setText(vocabs.get(0).getEnglish());
@@ -63,5 +71,23 @@ public class VocabulariesActivity extends BaseActivity {
         }
 
         binding.pbVocabulary.setProgress(currentPosition*5);
+    }
+
+    private void doAnimation() {
+        final ObjectAnimator oa1 = ObjectAnimator.ofFloat(binding.cvVocab, "scaleX", 1f, 0f);
+        final ObjectAnimator oa2 = ObjectAnimator.ofFloat(binding.cvVocab, "scaleX", 0f, 1f);
+        oa1.setInterpolator(new DecelerateInterpolator());
+        oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+        long durationAnimation = 170;
+        oa1.setDuration(durationAnimation);
+        oa2.setDuration(durationAnimation);
+        oa1.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                oa2.start();
+            }
+        });
+        oa1.start();
     }
 }
