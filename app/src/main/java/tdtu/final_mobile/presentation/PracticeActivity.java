0b3e6db0f91101ff4c87bfa51c915b0c0e8c5f84
hai <bongiuuu2020@gmail.com>
2021-11-15 14:02:42 +0700

    add retrofit to the project

--- app/src/main/java/tdtu/final_mobile/presentation/PracticeActivity.java
+++ app/src/main/java/tdtu/final_mobile/presentation/PracticeActivity.java
@@ -1,15 +1,53 @@
 package tdtu.final_mobile.presentation;
 
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import tdtu.final_mobile.data.response.MultipleResource;
 import tdtu.final_mobile.databinding.ActivityPracticeBinding;
+import tdtu.final_mobile.network.APIClient;
+import tdtu.final_mobile.network.APIService;
 
 public class PracticeActivity extends BaseActivity {
     private ActivityPracticeBinding binding;
-
+    APIService apiInterface;
     @Override
     public void doBusiness() {
+        apiInterface = APIClient.getClient().create(APIService.class);
+        Call<MultipleResource> call = apiInterface.doGetListResources();
+        call.enqueue(new Callback<MultipleResource>() {
+            @Override
+            public void onResponse(Call<MultipleResource> call, Response<MultipleResource> response) {
+
+                Log.d("TAG",response.code()+"");
+
+                String displayResponse = "";
+                MultipleResource resource = response.body();
+                Integer text = resource.page;
+                Integer total = resource.total;
+                Integer totalPages = resource.totalPages;
+                List<MultipleResource.Datum> datumList = resource.data;
+
+                displayResponse += text + " Page\n" + total + " Total\n" + totalPages + " Total Pages\n";
+
+                for (MultipleResource.Datum datum : datumList) {
+                    displayResponse += datum.id + " " + datum.name + " " + datum.pantoneValue + " " + datum.year + "\n";
+                }
+
+                binding.tvName.setText(displayResponse);
+
+            }
+
+            @Override
+            public void onFailure(Call<MultipleResource> call, Throwable t) {
+                call.cancel();
+            }
+        });
 
     }
 
@@ -19,13 +57,13 @@ public class PracticeActivity extends BaseActivity {
         return binding.getRoot();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityPracticeBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
-
-        binding.tvName.setText("Using Binding");
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        binding = ActivityPracticeBinding.inflate(getLayoutInflater());
+//        View view = binding.getRoot();
+//        setContentView(view);
+//
+//        binding.tvName.setText("Using Binding");
+//    }
 }
