d7270f08566cdf521c0883e2e0942715e59c94f0
hai <bongiuuu2020@gmail.com>
2021-11-23 13:22:52 +0700

    contribute questions

---
+++ app/src/main/java/tdtu/final_mobile/utils/Constants.java
@@ -0,0 +1,15 @@
+package tdtu.final_mobile.utils;
+
+public class Constants {
+    public static final String MONDAY = "Mon";
+    public static final String TUESDAY = "Tue";
+    public static final String WEDNESDAY = "Wed";
+    public static final String THURSDAY = "Thu";
+    public static final String FRIDAY = "Fri";
+    public static final String SATURDAY = "Sat";
+    public static final String SUNDAY = "Sun";
+    public static String CURRENT_DAY = "Mon";
+    public static String KEY_USER_NAME = "KEY_USER_NAME";
+    public static String KEY_USER_ID = "KEY_USER_ID";
+    public static String DEFAULT_PASSWORD = "123456";
+}
